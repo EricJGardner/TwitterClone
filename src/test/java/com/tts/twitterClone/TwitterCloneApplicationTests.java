9384688f8faf532bc7cec84deca687800355681a
EricJGardner <74524075+EricJGardner@users.noreply.github.com>
2021-02-16 08:53:49 -0600

    first commit

---
+++ src/test/java/com/tts/twitterClone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.twitterClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
